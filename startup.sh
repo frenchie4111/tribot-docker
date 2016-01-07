70804ca77ea85f8657aa0fc4b5d512e2bf10e07c
Michael Lyons <mdl0394@gmail.com>
2016-01-07 13:10:08 -0800

    Made tribot echo logs to tribot_logs file

--- startup.sh
+++ startup.sh
@@ -9,5 +9,7 @@ chmod 600 /root/.vnc/passwd
 vncserver :1 -geometry 1280x800 -depth 24
 
 # Run TRiBot
-DISPLAY=:1 XAUTHORITY=/root/.Xauthority java -jar TRiBot_Loader.jar
+DISPLAY=:1 XAUTHORITY=/root/.Xauthority java -jar TRiBot_Loader.jar 2>&1 >> tribot_logs.log
+
+tail -F /root/.vnc/*.log
 
